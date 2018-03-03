9d023e03b077dcfcf336c50405773e98b76f6b83
kSzpyt <karolo.szpyt@gmail.com>
2018-03-03 13:07:20 +0100

    wersja działa, wymagane poprawki estetyczne

--- Metoda-D-Hondta/server.R
+++ Metoda-D-Hondta/server.R
@@ -23,57 +23,69 @@ shinyServer(function(input, output) {
   })
   
   output$sumvotes <- renderText({
-      if(sum(a()[,2])>1)
-      {
-        x <- paste0(100 * sum(a()[,2]), "%"
-                    , " Suma sondaży nie może przekraczać 100%")
-      }
-      else
-      {
-        x <- paste0(100 * sum(a()[,2]), "%" 
-                    , " procent głosów. Do rozdysponowania między partie: "
-                    , 100 * (1 - sum(a()[,2])), "%")
-      }
-    })
+    if(sum(a()[,2])>1)
+    {
+      x <- paste0(100 * sum(a()[,2]), "%"
+                  , " Suma sondaży nie może przekraczać 100%")
+    }
+    else
+    {
+      x <- paste0(100 * sum(a()[,2]), "%" 
+                  , " procent głosów. Do rozdysponowania między partie: "
+                  , 100 * (1 - sum(a()[,2])), "%")
+    }
+  })
   
   output$votes <- renderTable({
-  boolvec <- sapply(v$data, function(x){# true false aby odrzucić 
-    if(x<0.05)#sondaże za małe
-    {
-      y <- FALSE
-     }
-      else
-      {
-         y <- TRUE
-       }
-     })
-     sounding <- v$data[boolvec]#odrzucamy <0.05
-     sounding <- sapply(sounding, function(x) rnorm(1, x, 0.1*x))#uwzględniamy błąd
-     boolvec2 <- sapply(sounding, function(x){# true false aby odrzucić 
+    sounding <- v$data
+    boolvec <- sapply(v$data, function(x){# true false aby odrzucić 
       if(x<0.05)#sondaże za małe
       {
-         y <- FALSE
+        y <- FALSE
       }
-        else
-       {
-                y <- TRUE
-     }
-     })
-    sounding <- sounding[boolvec]
-    
-    sounding <- rep(sounding * input$votes_count, times = 460)
-    mat <- matrix(sounding, ncol = input$slide1, byrow = TRUE)
+      else
+      {
+        y <- TRUE
+      }
+    })
+    sounding[boolvec == F] <- 0#odrzucamy <0.05
+    sounding <- v$data
+    sounding <- sapply(sounding, function(x) rnorm(1, x, 0.1*x))#uwzględniamy błąd
+    boolvec2 <- sapply(sounding, function(x){# true false aby odrzucić 
+      if(x<0.05)#sondaże za małe
+      {
+        y <- FALSE
+      }
+      else
+      {
+        y <- TRUE
+      }
+    })
+    sounding[boolvec2 == F] <- 0
+    #sounding <- c(sounding, 1 - sum(sounding))
+    sounding2 <- sounding
+    if((1 - sum(sounding2)) > 0)
+    {
+      x <- sounding2[1:length(sounding2)] * (1 - sum(sounding2))
+      sounding2 <- sounding2[1:length(sounding2)] + x
+    }
+    else
+    {
+      x <- sounding2[1:length(sounding2)] * (1 - sum(sounding2))
+      sounding2 <- sounding2[1:length(sounding2)] + x
+    }
+    sounding2 <- rep(sounding2 * input$votes_count, times = 460)
+    mat <- matrix(sounding2, ncol = input$slide1, byrow = TRUE)
     dfma <- as.data.frame(mat)
     divvec <- 1:460
     dfma <- dfma/divvec
     o <- order(dfma, decreasing = TRUE)[1:460]
     inv <- c(1:input$slide1*460)
-    tt <- findInterval(o, inv)
-    return(table(tt))
+    tt <- findInterval(o, inv) + 1
+    #return(data.frame(sounding2, sounding[1:length(sounding)], 1-sum(sounding)))
+    tt <- table(tt)
+    tt <- as.data.frame(tt)
+    colnames(tt) <- c("Patria", "Liczba głosów")
+    return(tt)
   })
 })
-
-
-
-
-
